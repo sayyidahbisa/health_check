780cc5f878ad13ba7312bf9994bf0342a539da1e
Sayyidah <sayyidahbisabikin@gmail.com>
2023-08-29 12:58:32 +0800

    Allowing printing more than one error message

--- all_check.py
+++ all_check.py
@@ -12,6 +12,7 @@ def check_disk_full(disk, min_gb, min_percent):
     du = shutil.disk_usage(disk)
     percent_free = 100 * du.free / du.total
     gigabytes_free = du.free / 2**30
+
     if percent_free < min_percent or gigabytes_free < min_gb:
         return True
     return False
@@ -25,12 +26,16 @@ def main():
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
     ]
+    everything_ok = True
     for check, msg in checks:
         if check():
             print(msg)
+            everything_ok = False
+
+    if not everything_ok:
             sys.exit(1)
     
-    print ("Everything is Okay!")
+    print ("Everything is Ok!")
     sys.exit(0)
-    
+
 main()
